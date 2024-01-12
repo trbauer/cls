f6dcf788f2227ed1aa547ee8726f17d09ff5ff2e
Tim Bauer <trbauer@gmail.com>
2024-01-12 01:10:41 -0600

    removed obsolete file

--- setup-vs2017.bat
+++
@@ -1,8 +0,0 @@
-mkdir builds\vs2017-64
-cd builds\vs2017-64
-cmake -G "Visual Studio 15 2017 Win64" ..\..
-cd ..\..
-mkdir builds\vs2017-32
-cd builds\vs2017-32
-cmake -G "Visual Studio 15 2017" ..\..
-cd ..\..
